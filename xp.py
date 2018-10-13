131ee03aa028602ca21fdf31f120ebd406bb8e11
Lickorice <infocarl45@gmail.com>
2018-10-13 20:19:38 +0800

    levelup picture done

--- xp.py
+++ xp.py
@@ -1,10 +1,17 @@
-import discord, datetime, asyncio, json
+import discord, datetime, asyncio, json, requests
+from PIL import Image
 from discord.ext import commands
 from discord.utils import get
 from data import ldb
+from io import BytesIO
 
 with open('assets/str_msgs.json') as f:
     str_messages = json.load(f)
+
+img_offset = 24, 24
+img_temp = Image.open('assets/img_level-up-template.png')
+bg_temp = Image.new('RGBA', (52, 52), (44, 47, 51, 255))
+bg2_temp = Image.open('assets/background-1.png')
 
 lkdb = ldb.LickDB()
 doctxt = "developed by Lickorice | Carlos Panganiban | cgpanganiban@up.edu.ph"
@@ -87,19 +94,17 @@ class Xp():
             if cur >= targetExp:
                 cur -= targetExp
                 newlvl = lkdb.updateLvl(a.id, residual=cur)
-                embed = discord.Embed(title="Level Up!", color=0xff1155)
-                embed.add_field(
-                    name="Name",
-                    value=a.name,
-                    inline=True
-                )
-                embed.add_field(
-                    name="Level",
-                    value=newlvl,
-                    inline=True
-                )
-                embed.set_thumbnail(url=a.avatar_url)
-                m = await self.bot.send_message(message.channel, embed=embed)
+                
+                # generate the image:
+                imgfile = requests.get(a.avatar_url)
+                pfp = Image.open(BytesIO(imgfile.content))
+                pfp = pfp.resize((52, 52), Image.BICUBIC)
+                bg_temp.paste(pfp, (0, 0))
+                img_temp.paste(bg_temp, img_offset, mask=bg_temp)
+                bg2_temp.paste(img_temp, (0,0), mask=img_temp)
+                bg2_temp.save('temp/levelup.png')
+                
+                m = await self.bot.send_file(message.channel, 'temp/levelup.png')
                 await asyncio.sleep(10)
                 await self.bot.delete_message(m)
             print(a.id, cur, targetExp)
