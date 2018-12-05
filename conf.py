6f5896e2a64a6e4cef5acf878ae1160f5f3fb11f
Carlos Fernando G. Panganiban <infocarl45@gmail.com>
2018-12-06 01:45:26 +0800

    debug hotfix

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ class Config():
     def __init__(self):
         self.OWNER_ID = 319285994253975553
         self.OWNER_GUILD_ID = 378540712653750274
-        self.OWNER_GUILD_ID = 442676019774750731 # uncomment for development
+        # self.OWNER_GUILD_ID = 442676019774750731 # uncomment for development
 
         self.VIP_ROLE_ID = 444142897290412053
         self.VIP_CHANNEL_ID = 444150135664934912
